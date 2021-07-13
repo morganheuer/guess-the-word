6ff7c34227df99d2c4408e7ae29f98642ad7f08f
morganheuer <morganheuer@gmail.com>
2021-07-13 16:29:36 -0700

    Created global variables, added placeholder for letters, added event listener for button

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuessesElement = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+// Display dot placeholders for the chosen word's letters
+const placeholder = function(word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
+guessButton.addEventListener("click", function(e){
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value = "";
+});
